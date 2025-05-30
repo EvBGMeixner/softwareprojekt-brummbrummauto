eb8150a415a061a025522f9a10605e9273b36e19
Paul <paul.kania@t-online.de>
2025-05-30 10:43:50 +0200

    zu

--- FIGUR1.java
+++ FIGUR1.java
@@ -1,5 +1,12 @@
-public class FIGUR1{
+public class FIGUR1 extends FIGUR{
     private String name; 
     private int leben; 
-    private int 
+    private int x; 
+    private int y; 
+    private int Geschwindigkeit; 
+public FIGUR1 (String name, int leben, int x, int y){
+    this.
+    
 }
+}
+
--- Jump.java
+++ Jump.java
@@ -3,9 +3,5 @@
 public class Jump extends SPIEL
 {
     
-    public Jump()
-    {
-        
-    }
-
+    
 }
