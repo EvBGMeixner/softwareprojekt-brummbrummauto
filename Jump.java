cca19f4c6756d6cfd602f5920e7ad7d63702c918
Noah <noah.roehrich@student.evbg.de>
2025-06-03 09:39:29 +0200

    boden ist da

--- Jump.java
+++ Jump.java
@@ -2,14 +2,41 @@
 public class Jump extends SPIEL
 {
     
-    
+    RECHTECK[] boden;
     
     
     public Jump()
     {
         super();
         
-        LEVEL l1=new LEVEL1();
-
+        LEVEL1();
+        zeigeKoordinatensystem(true);
+    }
+    
+    
+    
+    public void LEVEL1()
+    {
+        boden=new RECHTECK[7];
+        
+        for (int i=0; i< boden.length;i++){
+            boden[i]=new RECHTECK();
+            boden[i].setzeGroesse(5,2);
+            boden[i].setzeFarbe("weiß");
+            
+            
+        }
+        
+        boden[0].setzeMittelpunkt(-15, -10);
+        boden[1].setzeMittelpunkt(-10, -10);
+        boden[2].setzeMittelpunkt(-5, -10);
+        boden[3].setzeMittelpunkt(0, -10);
+        boden[4].setzeMittelpunkt(5, -10);
+        boden[5].setzeMittelpunkt(10, -10);
+        boden[6].setzeMittelpunkt(15, -10);
+        
+        
+                
+       
     }
 }
