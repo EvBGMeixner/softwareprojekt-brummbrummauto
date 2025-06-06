48a3f19c80a7a160d07daa7b3398f3f318fefa6b
Paul <paul.kania@t-online.de>
2025-06-06 09:01:45 +0200

    u

---
+++ Figur1.java
@@ -0,0 +1,11 @@
+public class Figur1 extends FIGUR {
+    int posx; 
+    int posy; 
+    int leben; 
+    String name;
+    private Figur1()  
+        
+    }
+}
+
+
--- Jump.java
+++ Jump.java
@@ -1,1 +1,46 @@
 
+<<<<<<< HEAD
+=======
+public class Jump extends SPIEL
+{
+    
+    RECHTECK[] boden;
+    
+    //Hallo ich heiße Noah;
+    public Jump()
+    {
+        super();
+        
+        LEVEL1();
+        zeigeKoordinatensystem(true);
+        Figur1(); 
+    }
+    
+    
+    
+    public void LEVEL1()
+    {
+        boden=new RECHTECK[7];
+        
+        for (int i=0; i< boden.length;i++){
+            boden[i]=new RECHTECK();
+            boden[i].setzeGroesse(5,2);
+            boden[i].setzeFarbe("weiß");
+            
+            
+        }
+        
+        boden[0].setzeMittelpunkt(-15, -10);
+        boden[1].setzeMittelpunkt(-10, -10);
+        boden[2].setzeMittelpunkt(-5, -10);
+        boden[3].setzeMittelpunkt(0, -10);
+        boden[4].setzeMittelpunkt(5, -10);
+        boden[5].setzeMittelpunkt(10, -10);
+        boden[6].setzeMittelpunkt(15, -10);
+        
+        
+                
+       
+    }
+}
+>>>>>>> 5cfad6cfc1806fc353372997586ad865c0b53ef3
